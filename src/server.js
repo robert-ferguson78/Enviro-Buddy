8ba13b24f60a5f384765580ef4add63cf1fa8aa2
Robert Ferguson <robert.ferguson@digitalstrategy.ie>
2024-02-23 22:32:37 +0000

    server js update

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ Object.keys(handlebarsHelpers).forEach((helper) => {
 
 async function init() {
   const server = Hapi.server({
-    port: 3000,
+    port: process.env.PORT || 3000,
     host: "localhost",
   });
 
