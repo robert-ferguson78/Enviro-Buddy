5d6caa3c001a213e538d3b0a684c02d687bfe865
Robert Ferguson <robertferguson78@gmail.com>
2024-03-05 16:25:26 +0000

    add helper function to check for user type

--- src/helpers/handlebars-helpers.js
+++ src/helpers/handlebars-helpers.js
@@ -1,9 +1,11 @@
 export const handlebarsHelpers = {
-        modAdd1: function(index, value, options) {
-            if ((parseInt(index, 10) + 1) % value === 0) {
-                return options.fn(this);
-            }
-                return options.inverse(this);
-        },
-        
-    };
+    modAdd1: function(index, value, options) {
+        if ((parseInt(index, 10) + 1) % value === 0) {
+            return options.fn(this);
+        }
+            return options.inverse(this);
+    },
+    eq: function(v1, v2) {
+        return v1 === v2;
+    },  
+};
